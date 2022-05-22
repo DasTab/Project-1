3bf7fe9ac1d47f8cb7382627da5a9a9ec3b4f208
DasTab <dtabaldiev@gmail.com>
2022-05-22 21:53:07 +0600

    used conditions and loops

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -7,16 +8,35 @@ const personalMovieDB = {
   genres: [],
   private: false,
 };
- 
-const a = prompt("Последний просмотренный фильм?", ""),
-  b = +prompt("На сколько вы его оцените?", ""),
-  c = prompt("Последний просмотренный фильм?", ""),
-  d = +prompt("На сколько вы его оцените?", "");
- 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
- 
-// если вносим данные через точку то возможно данные не правильно отобразятся personalMovieDB.movies.a = b, 
-// поэтому вносим данные через квадратные скобки personalMovieDB.movies[c] = d; 
- 
+
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Последний просмотренный фильм?", ""),
+        b = prompt("На сколько вы его оцените?", "");
+  if (a.length < 50 && a != null && b != null && a != "" && b != "") {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
+if (personalMovieDB.count >=0 && personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
+
+
+// // если вносим данные через точк10у то возможно данные не правильно отобразятся personalMovieDB.movies.a = b,
+// // поэтому вносим данные через квадратные скобки personalMovieDB.movies[c] = d;
+
 console.log(personalMovieDB);
+
+
+
